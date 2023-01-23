e59b18916940c20a587047507a1ed7ea5145daca
tessc14 <tessc14@gmail.com>
2023-01-23 18:13:53 +0000

    display random flags

--- flags.js
+++ flags.js
@@ -1,9 +1,11 @@
 const url_base = "https://geo-genius-server.onrender.com/";
 
+//this works
 const getFlagButton = document.getElementById("button");
 
-async function displayFlag() {
+async function displayFlag(e) {
     
+    e.preventDefault();
     const response = await fetch(url_base + "flag-facts/random")
     const flag = await response.json()
   
@@ -12,11 +14,20 @@ async function displayFlag() {
     const flagFact = document.querySelector("#facts");
 
     flagImage.src = flag["flagFile"];
+    //make this a hint or change the fact to not include country name or appear when guessed correctly
     flagFact.textContent = flag["interestingFact"];
 
     //assign country of flag to correct answer to be used with guess button + form
     const correctAnswer = flag["countryName"];
     return correctAnswer;
   }
+
+  //started function for when guess is clicked
+//   const guess = document.getElementById("form");
+
+//   async function guessAnswer(e) {
+
+//   }
+
 
  getFlagButton.addEventListener("click", displayFlag);
