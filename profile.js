0b8b4f409db707cee13e1eeeb8e7b1347ff21df8
Richard Swainson <rich_swainson@hotmail.co.uk>
2023-01-23 19:17:09 +0000

    Add comments to profile.js and get random fun fact upon loading index.html

--- profile.js
+++ profile.js
@@ -2,19 +2,24 @@ const url_base = "https://geo-genius-server.onrender.com/";
 
 const submitNameButton = document.getElementById("submit-name-form");
 
+// event listener function that gets added to submit button
 async function submitName(e) {
     e.preventDefault();
 
+    // if no name entered into box, return
     let name = e.target.userNameInput.value;
     if (!name) {
         return;
     }
 
+    // capitalize the name
     name = name.charAt(0).toUpperCase() + name.slice(1);
 
+    // get current userProfile data
     let userProfile = await getUserProfile();
 
     if (!updateUserName(name)) {
+        // error updating the same for same reason
         console.log("Name not changed");
         return;
     }
@@ -23,12 +28,15 @@ async function submitName(e) {
     document.getElementById("userNameInput").value = "";
 
     if (userProfile.name) {
+        // there was already name in database
         alert("Name succesfully changed!");
         return;
     }
+    // name added to database
     alert("Name succesfully added!");
 }
 
+// call the PUT user endpoint to edit the userProfile data
 async function updateUserName(name) {
     const options = {
         method: "PUT",
@@ -41,9 +49,11 @@ async function updateUserName(name) {
 
     let response = await fetch(url_base + "user", options);
 
+    // return true if worked, else return false
     return response.status === 200;
 }
 
+// call GET user endpoint, to get userProfile data
 async function getUserProfile() {
     let res = await fetch(url_base + "user");
 
@@ -56,6 +66,7 @@ async function getUserProfile() {
     return userProfile;
 }
 
+// update the name and points in the html document
 function updateNameHTML(name) {
     const userNameElements = document.getElementsByClassName("user-name");
     for (e of userNameElements) {
@@ -68,8 +79,8 @@ function updateNameHTML(name) {
 }
 
 const randomiseButton = document.getElementById("randomise-button");
-console.log(randomiseButton);
 
+// event listener function added to randomise button to get a random fun fact
 async function newRandomFact() {
     const randomFact = await getRandomFact();
 
@@ -79,6 +90,7 @@ async function newRandomFact() {
     }
 }
 
+// call GET random fact endpoint
 async function getRandomFact() {
     const res = await fetch(url_base + "fun-facts/random");
 
@@ -89,6 +101,8 @@ async function getRandomFact() {
 
     return await randomFact;
 }
+
+newRandomFact();
 
 submitNameButton.addEventListener("submit", submitName);
 randomiseButton.addEventListener("click", newRandomFact);
