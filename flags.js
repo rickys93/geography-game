79bc86650973ab725d2e904d06ab961cf51a62f3
tessc14 <tessc14@gmail.com>
2023-01-24 10:50:28 +0000

    display correct when correct

--- flags.js
+++ flags.js
@@ -2,7 +2,10 @@ const url_base = "https://geo-genius-server.onrender.com/";
 
 //this works
 const getFlagButton = document.getElementById("button");
-const submitGuess = document.getElementById("form");
+// const submitGuess = document.getElementById("form");
+const guessButton = document.getElementById("form");
+const correctAnswer = document.getElementById("correctAnswer");
+const correctHeading = document.getElementById("correct");
 
 async function displayFlag(e) {
     
@@ -17,31 +20,31 @@ async function displayFlag(e) {
     flagImage.src = flag["flagFile"];
     //make this a hint or change the fact to not include country name or appear when guessed correctly
     flagFact.textContent = flag["interestingFact"];
-
+    
     //assign country of flag to correct answer to be used with guess button + form
-    const correctAnswer = flag["countryName"];
-    return correctAnswer;
+    correctAnswer.textContent = flag["countryName"];
+    
   }
 
   //started function for when guess is clicked
-const guess = document.getElementById("form");
-
 async function guessAnswer(e) {
 
-    e.preventDefault();
-
-    let guess = e.target.flag-guess.value;
-    // if (!guess) {
-    //     return;
-    // }
-
-    guess = guess.charAt(0).toUpperCase() + guess.slice(1);
-
-    if (guess === correctAnswer) {
-        console.log("correct")
+        e.preventDefault();
+    
+        let userGuess = e.target.flagGuess.value;
+        
+        // if (!guess) {
+        //     return;
+        // }
+    
+        userGuess = userGuess.charAt(0).toUpperCase() + userGuess.slice(1);
+    
+        if (userGuess === correctAnswer.textContent) {
+             correctHeading.textContent = "CORRECT!";
+        }
+    
     }
 
-}
+guessButton.addEventListener("submit", guessAnswer);
 
-submitGuess.addEventListener("submit", guessAnswer);
 getFlagButton.addEventListener("click", displayFlag);
