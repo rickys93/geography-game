708e1c9b6903a8b81305dd54b308a537f8ddf4df
tessc14 <tessc14@gmail.com>
2023-01-24 15:51:51 +0000

    creat hint option

--- js/flags.js
+++ js/flags.js
@@ -1,14 +1,19 @@
 let url_base = "https://geo-genius-server.onrender.com/";
 
-//this works
 const getFlagButton = document.getElementById("button");
 const guessButton = document.getElementById("form");
+//hidden element to store country of flag to compare to user guess
 const correctAnswer = document.getElementById("correctAnswer");
+//hidden element to store fact related to flag
 const funFact = document.getElementById("funFact");
+//heading to appear when user guesses correctly
 const correctHeading = document.getElementById("correct");
+//what flag is this heading to appear when user clicks new flag/click to play
 const whatFlagHeader = document.getElementById("whatFlag");
-const flagFact = document.querySelector("#facts");
+//hint when clicked should display fact related to country
+const flagHint = document.querySelector("#hint");
 
+//function to display a random flag from API and store the relative country and fact
 async function displayFlag(e) {
     e.preventDefault();
     const response = await fetch(url_base + "flag-facts/random");
@@ -22,16 +27,16 @@ async function displayFlag(e) {
 
     flagImage.src = flag["flagFile"];
     //will appear when guessed correctly
-    funFact.textContent = `Fun Fact: ${flag["interestingFact"]}`;
+    funFact.textContent = `Hint: ${flag["interestingFact"]}`;
 
     correctHeading.textContent = "";
-    flagFact.textContent = "A fun fact about the country will appear when you guess correctly!"
+    flagHint.textContent = "Click to reveal a hint"
     
     //assign country of flag to correct answer to be used with guess button + form
     correctAnswer.textContent = flag["countryName"];
 }
 
-//started function for when guess is clicked
+//function for when guess is clicked
 async function guessAnswer(e) {
     e.preventDefault();
 
@@ -41,17 +46,28 @@ async function guessAnswer(e) {
     // }
     userGuess = userGuess.charAt(0).toUpperCase() + userGuess.slice(1);
 
-    if (userGuess === correctAnswer.textContent) {
-        correctHeading.textContent = "CORRECT!";
-        flagFact.textContent = funFact.textContent;
-    } else if (userGuess.length === 0) {
+    //if their guess is the same as stored correct answer for flag then show correct
+    if (userGuess.length === 0) {
         correctHeading.textContent = "Please enter a guess";
+    } else if (userGuess === correctAnswer.textContent) {
+        correctHeading.textContent = "CORRECT!";
+        
     } else {
         correctHeading.textContent = `WRONG! The correct answer was ${correctAnswer.textContent}`;
     }
 
     document.getElementById("flagGuess").value = "";
 }
+
+async function showHint(e) {
+    e.preventDefault();
+
+    flagHint.textContent = funFact.textContent;
+
+}
+
+
+flagHint.addEventListener("click", showHint);
 
 guessButton.addEventListener("submit", guessAnswer);
 
